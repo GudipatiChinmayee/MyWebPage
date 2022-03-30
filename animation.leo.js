8008c9f7f9f9a9251e120d87179c398a636daef7
Chinmayee Gudipati <96588904+GudipatiChinmayee@users.noreply.github.com>
2022-03-30 21:55:00 +0530

    Add files via upload
    chinmayeesharma.glitch.me/

---
+++ animation.leo.js
@@ -0,0 +1,68 @@
+let c=document.getElementId("my-canvas");
+let ctx= c.getContext("2d");
+
+let loadImage=(src,callback)=> {
+ let img = document.createElement("img");
+ img.onload={} => callback(img);
+ img.src=src;
+}
+
+let imagePath= (frameNumber,animation)=> {
+ return ""+animation +"/"+frameNumber+".png";
+}
+let frames={
+ idle:[1,2,3,4,5,6,7,8],
+ kick:[1,2,3,4,5,6,7],
+ pucnh:[1,2,3,4,5,6,7],
+}
+let loadImages=(callback)=>{
+ let images={idle:[], kick:[], punch:[]};
+ let imagesToLoad=0;
+ ["idle", "kick", "punch"].forEach(animation)=>{
+  let animationFrames=frames[animation];
+  imagesToLoad=imagesToLoad+ aniamtionFrames.length;
+  aniamtionFrames.forEach(frameNumber)=>{
+   let path=imagePath(frameNumber,animation);
+   loadImage(path, (image)=> {
+   images[animation][frameNumber-1]= image;
+   imagesToLoad=imagesToLoad-1;
+
+   if (imagesToLoad===0){
+    callback(images);
+  let imagesToLoad=8;
+   }
+  })
+ })
+})
+}
+
+let animate=(ctx, images,animation, callback)=>{
+ images[animation].forEach(image,index)=>{
+  setTimeout(()=>{
+   ctx.clearRect(0,0,500,500);
+   ctx.drawImage(image, 0, 0 , 500, 500);
+  }, index*100);
+})
+ setTimeout(callback, images[animation].length*100);
+}
+loadImages(images )=>{
+ let queuedAnimations=[];
+
+ let aux =() => {
+  let selectedAnimation;
+  if (queuedAnimations.length===0){
+   selectedAnimation="idle"
+  }else{
+   selectedAnimation=queuedAnimations.shift();
+  }
+
+  animate(ctx, images,selectedAnimation,aux)
+}
+ aux();
+ document.getElementById("kick").onclick = ()=>{
+  queuedAnimations.push("kick");
+}
+document.getElementById("punch").onclick = ()=>{
+ queuedAnimations("punch");
+}
+})
